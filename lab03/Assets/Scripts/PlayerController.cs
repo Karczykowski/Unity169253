d8ee46c148389e31de400503f37c6b89738b1c9c
Karczykowski <mkarczykowski@gmail.com>
2025-10-20 12:04:13 +0200

    lab03

---
+++ lab03/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    private bool isJumping = false;
+    public float speed = 5;
+
+    // Backing field for health
+    [SerializeField]
+    private int health = 100;
+
+    // Property to control access to health
+    public int Health
+    {
+        get { return health; }
+        set
+        {
+            // Ensure health is within a valid range
+            health = Mathf.Clamp(value, 0, 100);
+            UnityEngine.Debug.Log("Player Health: " + health);
+        }
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void OnJump(InputValue movementValue)
+    {
+        rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        Health -= 10;
+
+    }
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ lab03/Assets/Scripts/RequireAndDisallowExample.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Rigidbody))]
+public class RequireAndDisallowExample : MonoBehaviour
+{
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            UnityEngine.Debug.Log("Rigidbody component is present as required.");
+        }
+        else
+        {
+            UnityEngine.Debug.LogError("Rigidbody component is missing, which should not happen due to [RequireComponent].");
+        }
+    }
+}
+
+/*
+ * Explanation of Attributes:
+ * - [RequireComponent(typeof(Component))]: Forces the GameObject to have the specified component before adding this script.
+ * - [DisallowMultipleComponent]: Prevents multiple instances of the same script from being added to a single GameObject.
+ */
